7cd59a6ef8003feb3c02a4d52891c94601f9f5a2
amitchatterjee <amit.chatterjee@quik-j.com>
2016-03-04 21:10:55 -0500

    Fixed bugs found in the operator DND enhancements

--- ace-app/src/main/java/com/quikj/application/web/talk/feature/operator/OperatorMBean.java
+++ ace-app/src/main/java/com/quikj/application/web/talk/feature/operator/OperatorMBean.java
@@ -7,13 +7,13 @@ package com.quikj.application.web.talk.feature.operator;
  * @author amit
  *
  */
-public interface OperatorManagementMBean {
+public interface OperatorMBean {
 
 	public static final String MBEAN_SUFFIX = "com.quikj.application.web.talk.feature.operator:type=Operator-";
 	
-	public String getOperatorAvailableQueueSize();
-	public String getOperatorsWithDNDSize();
-	public String getSubscriberQueueSize();
-	public String getOperatorSummary();
+	public int getOperatorAvailableQueueSize();
+	public int getOperatorsWithDNDSize();
+	public int getSubscriberQueueSize();
+	public String  getOperatorSummary();
 	public String getVisitorSummary();
 }
