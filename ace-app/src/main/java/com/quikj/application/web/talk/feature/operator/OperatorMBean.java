50acf1d7104d3e2d08ba64e12f29cbeb5961866c
amitchatterjee <amit.chatterjee@quik-j.com>
2016-03-12 22:14:47 -0500

    Minor changes to the operator feature

--- ace-app/src/main/java/com/quikj/application/web/talk/feature/operator/OperatorMBean.java
+++ ace-app/src/main/java/com/quikj/application/web/talk/feature/operator/OperatorMBean.java
@@ -3,6 +3,8 @@
  */
 package com.quikj.application.web.talk.feature.operator;
 
+import java.util.Date;
+
 /**
  * @author amit
  *
@@ -10,10 +12,16 @@ package com.quikj.application.web.talk.feature.operator;
 public interface OperatorMBean {
 
 	public static final String MBEAN_SUFFIX = "com.quikj.application.web.talk.feature.operator:type=Operator-";
-	
-	public int getOperatorAvailableQueueSize();
-	public int getOperatorsWithDNDSize();
-	public int getSubscriberQueueSize();
-	public String  getOperatorSummary();
-	public String getVisitorSummary();
+
+	int getOperatorAvailableQueueSize();
+
+	int getOperatorsWithDNDSize();
+
+	int getSubscriberQueueSize();
+
+	String getOperatorSummary();
+
+	String getVisitorSummary();
+
+	Date getPausedUntil();
 }
