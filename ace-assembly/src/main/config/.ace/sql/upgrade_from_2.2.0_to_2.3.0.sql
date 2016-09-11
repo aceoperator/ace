3f32c4067bdd037c9f52e4db09e96a0c6fc1e9e6
amitchatterjee <amit.chatterjee@quik-j.com>
2016-09-11 18:04:51 -0400

    Modified the upgrade SQL statement

--- ace-assembly/src/main/config/.ace/sql/upgrade_from_2.2.0_to_2.3.0.sql
+++ ace-assembly/src/main/config/.ace/sql/upgrade_from_2.2.0_to_2.3.0.sql
@@ -2,4 +2,5 @@ USE webtalk;
 
 -- Change on 3/8/2016
 -- do it for all groups
-insert into feature_params_tbl values (XX, 'display-wait-time', 'false');
+
+insert into feature_params_tbl(feature_id, pname, pvalue) select id, 'display-wait-time', 'false' from feature_tbl;
