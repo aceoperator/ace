5fae57b6eb53170439facea8ae6231acddd1a904
Amit Chatterjee <amit.chatterjee@quik-j.com>
2016-11-16 08:32:47 -0500

    Removed the hard-coding of user.home location. Instead using ace.root.dir as the property for locating aceoperator configuration.

--- ace-framework/src/main/java/com/quikj/server/framework/AceConfigFileHelper.java
+++ ace-framework/src/main/java/com/quikj/server/framework/AceConfigFileHelper.java
@@ -21,32 +21,9 @@ public class AceConfigFileHelper {
 	}
 
 	public static String getAceRoot() {
-		String aceHome = System.getProperty("ace.root");		
-		if (aceHome == null) {
-			aceHome = System.getProperty("user.home");
-			if (!new File(aceHome + "/.ace").isDirectory()) {
-				File[] root = File.listRoots();
-				if (root == null) {
-					// Should not come here
-					root = new File[1];
-					root[0] = new File("/");
-				}
-
-				boolean found = false;
-				for (File rootElement : root) {
-					aceHome = rootElement.getAbsolutePath() + "/usr/share/aceoperator";
-					if (new File(aceHome + "/.ace").isDirectory()) {
-						found = true;
-						break;
-					}
-				}
-				
-				if (!found) {
-					throw new AceRuntimeException("The root directory could not be determined");
-				}
-			}
-		} else if (!new File(aceHome + "/.ace").isDirectory()) {
-			throw new AceRuntimeException("The ace.root property is not right");
+		String aceHome = System.getProperty("ace.root.dir");		
+		if (aceHome == null || !new File(aceHome + "/.ace").isDirectory()) {
+			throw new AceRuntimeException("The ace.root.dir property is not pointing to the right location");
 		}
 		
 		return aceHome;
