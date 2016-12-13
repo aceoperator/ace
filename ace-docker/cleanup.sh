ee04a29b5bf2c6c642070f7d8c59c43f973e47b1
Amit Chatterjee <amit.chatterjee@quik-j.com>
2016-12-13 09:06:42 -0500

    docker support work completed and tested

--- ace-docker/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker rmi aceoperator/frontend:latest aceoperator/frontend:2.3.0-SNAPSHOT \
-	aceoperator/app:latest aceoperator/app:2.3.0-SNAPSHOT \
-	aceoperator/mail:latest aceoperator/mail:2.3.0-SNAPSHOT \
-	aceoperator/db:latest aceoperator/db:2.3.0-SNAPSHOT
-	
-docker images
