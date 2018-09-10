27c7277aa7b8b064511e969164b2b3e7d0f20cde
Amit Chatterjee <amit.chatterjee@quik-j.com>
2018-09-10 16:27:16 +0000

    breaking up the database script to be more docker friendly

--- ace-assembly/src/main/config/.ace/sql/init_objects.sql
+++ ace-assembly/src/main/config/.ace/sql/init_objects.sql
@@ -1,21 +1,13 @@
-
-use mysql;
-
-GRANT ALL PRIVILEGES ON webtalk.* TO $$ACE(ACE_SQL_USER)@localhost IDENTIFIED BY '$$ACE(ACE_SQL_PASSWORD)';
-GRANT ALL PRIVILEGES ON webtalk.* TO $$ACE(ACE_SQL_USER)@localhost.localdomain IDENTIFIED BY '$$ACE(ACE_SQL_PASSWORD)';
-GRANT ALL PRIVILEGES ON webtalk.* TO $$ACE(ACE_SQL_USER)@'%' IDENTIFIED BY '$$ACE(ACE_SQL_PASSWORD)';
-
-CREATE DATABASE webtalk;
 USE webtalk;
 
-CREATE TABLE `account_tbl` (
+CREATE TABLE IF NOT EXISTS `account_tbl` (
   userid VARCHAR(40) NOT NULL,
   password VARCHAR(255) NOT NULL,
   addnl_info VARCHAR(128) DEFAULT '',
   PRIMARY KEY (userid)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  log_tbl
+CREATE TABLE IF NOT EXISTS  log_tbl
    (dated   TIMESTAMP NOT NULL,
     level   VARCHAR(10) NOT NULL,
     message VARCHAR(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -25,7 +17,7 @@ CREATE TABLE  log_tbl
     KEY (process)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  group_tbl (
+CREATE TABLE IF NOT EXISTS  group_tbl (
   groupid varchar(64) binary NOT NULL default '',
   domain varchar(40) default '',
   flags int default '0',
@@ -36,7 +28,7 @@ CREATE TABLE  group_tbl (
   PRIMARY KEY (groupid)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  user_tbl (
+CREATE TABLE IF NOT EXISTS  user_tbl (
   id bigint unsigned NOT NULL AUTO_INCREMENT,
   userid varchar(40) binary NOT NULL default '',
   password varchar(255) binary default '',
@@ -56,7 +48,7 @@ CREATE TABLE  user_tbl (
   UNIQUE KEY (userid)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE group_owner_tbl (
+CREATE TABLE IF NOT EXISTS group_owner_tbl (
   `userid` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   `groupid` varchar(64) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   PRIMARY KEY (`groupid`),
@@ -67,7 +59,7 @@ CREATE TABLE group_owner_tbl (
   CONSTRAINT `FK2_group_owner_tbl` FOREIGN KEY (`groupid`) REFERENCES `group_tbl` (`groupid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE group_member_tbl (
+CREATE TABLE IF NOT EXISTS group_member_tbl (
   `userid` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   `groupid` varchar(64) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   UNIQUE KEY `UK1_group_member_tbl` (`userid`,`groupid`),
@@ -80,7 +72,7 @@ CREATE TABLE group_member_tbl (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `blacklist_tbl` (
+CREATE TABLE IF NOT EXISTS `blacklist_tbl` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
   `identifier` varchar(100) NOT NULL,
@@ -93,14 +85,14 @@ CREATE TABLE `blacklist_tbl` (
   CONSTRAINT `blacklist_tbl_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_tbl` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_logout_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_logout_tbl (
   loginid varchar(255) binary NOT NULL default '',
   time_stamp datetime NOT NULL default 0,
   index loginid (loginid(100)),
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_reg_login_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_reg_login_tbl (
   loginid varchar(255) binary NOT NULL default '',
   username varchar(40) binary NOT NULL default '',
   time_stamp datetime NOT NULL default 0,
@@ -108,7 +100,7 @@ CREATE TABLE  cdr_reg_login_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_disc_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_disc_tbl (
   session varchar(255) binary NOT NULL default '',
   code int NOT NULL default 0,
   time_stamp datetime NOT NULL default 0,
@@ -116,7 +108,7 @@ CREATE TABLE  cdr_session_disc_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_join_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_join_tbl (
   first_session varchar(255) binary NOT NULL default '',
   time_stamp datetime NOT NULL default 0,
   more_sessions blob,
@@ -124,7 +116,7 @@ CREATE TABLE  cdr_session_join_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_leave_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_leave_tbl (
   session varchar(255) binary NOT NULL default '',
   endpoint varchar(255) binary NOT NULL default '',
   time_stamp datetime NOT NULL default 0,
@@ -132,7 +124,7 @@ CREATE TABLE  cdr_session_leave_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_setup_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_setup_tbl (
   session varchar(255) binary NOT NULL default '',
   calling varchar(255) binary NOT NULL default '',
   called varchar(255) binary NOT NULL default '',
@@ -142,7 +134,7 @@ CREATE TABLE  cdr_session_setup_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_setup_resp_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_setup_resp_tbl (
   session varchar(255) binary NOT NULL default '',
   loginid varchar(255) binary NOT NULL default '',
   status smallint NOT NULL default 0,
@@ -152,7 +144,7 @@ CREATE TABLE  cdr_session_setup_resp_tbl (
   index status (status)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_session_transfer_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_session_transfer_tbl (
   session varchar(255) binary NOT NULL default '',
   transferto varchar(255) binary NOT NULL default '',
   identifier varchar(255) binary NOT NULL default '',
@@ -161,7 +153,7 @@ CREATE TABLE  cdr_session_transfer_tbl (
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  cdr_unreg_login_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_unreg_login_tbl (
   loginid varchar(255) binary NOT NULL default '',
   name varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci default '',
   address varchar(255) binary default '',
@@ -175,14 +167,14 @@ CREATE TABLE  cdr_unreg_login_tbl (
 
 -- TODO change the column name groupid to group or owner (the column contains a user name
 -- who is an owner of a group
-CREATE TABLE  cdr_unreg_opbusy_tbl (
+CREATE TABLE IF NOT EXISTS  cdr_unreg_opbusy_tbl (
   groupid varchar(64) binary NOT NULL,
   time_stamp datetime NOT NULL default 0,
   index groupid (groupid),
   index timestamp (time_stamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  opm_operator_tbl (
+CREATE TABLE IF NOT EXISTS  opm_operator_tbl (
   groupid varchar(64) binary NOT NULL default '',
   time_stamp datetime NOT NULL default 0,
   opm_name varchar(64) binary NOT NULL default '',
@@ -192,7 +184,7 @@ CREATE TABLE  opm_operator_tbl (
   KEY (opm_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE canned_message_tbl
+CREATE TABLE IF NOT EXISTS canned_message_tbl
 	(id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	grp VARCHAR(50) BINARY NULL,
 	description VARCHAR(100) CHARACTER SET utf8 NOT NULL default '',
@@ -202,7 +194,7 @@ CREATE TABLE canned_message_tbl
 	CONSTRAINT `cannedmsgtbl_fk_1` FOREIGN KEY (grp) REFERENCES group_tbl (groupid) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  feature_tbl (
+CREATE TABLE IF NOT EXISTS  feature_tbl (
   id int unsigned NOT NULL AUTO_INCREMENT,
   fname varchar(40) binary NOT NULL,
   domain varchar(40) default '',
@@ -212,7 +204,7 @@ CREATE TABLE  feature_tbl (
   UNIQUE (fname)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `feature_params_tbl` (
+CREATE TABLE IF NOT EXISTS `feature_params_tbl` (
   `feature_id` int(10) unsigned NOT NULL DEFAULT '0',
   `pname` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   `pvalue` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
@@ -222,7 +214,7 @@ CREATE TABLE `feature_params_tbl` (
   	ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE  user_security_questions_tbl (
+CREATE TABLE IF NOT EXISTS  user_security_questions_tbl (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
   question_id TINYINT(4) NOT NULL ,
   question_value VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
@@ -233,7 +225,7 @@ CREATE TABLE  user_security_questions_tbl (
   FOREIGN KEY (user_id) REFERENCES user_tbl(id) ON DELETE CASCADE 
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE form_tbl (
+CREATE TABLE IF NOT EXISTS form_tbl (
   id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   session BIGINT UNSIGNED NOT NULL,
   canned_msg_id BIGINT UNSIGNED NOT NULL,
@@ -243,32 +235,32 @@ CREATE TABLE form_tbl (
   INDEX(send_time)
 ) ENGINE = InnoDB;
   
-INSERT INTO account_tbl (userid, password, addnl_info) VALUES ('ace', PASSWORD('a1b2c3d4'), 'Ace Super User');
+INSERT IGNORE INTO account_tbl (userid, password, addnl_info) VALUES ('ace', PASSWORD('a1b2c3d4'), 'Ace Super User');
 
-INSERT INTO group_tbl VALUES ('operator-group','ace',0,3,3,0,0);
-INSERT INTO user_tbl (userid, password, domain, flags, fullname, address, addnl_info, unavail_xferto, gatekeeper, password_updated)
+INSERT IGNORE INTO group_tbl VALUES ('operator-group','ace',0,3,3,0,0);
+INSERT IGNORE INTO user_tbl (userid, password, domain, flags, fullname, address, addnl_info, unavail_xferto, gatekeeper, password_updated)
 	VALUES ('operator',password('a1b2c3d4'),'ace',0,'Ace Operator group owner','','Owner of operator group','messagebox', '', NOW());
-INSERT INTO group_owner_tbl values ('operator','operator-group');
-INSERT INTO user_tbl (userid, password, domain, flags, fullname, address, addnl_info, unavail_xferto, gatekeeper, password_updated) 
+INSERT IGNORE INTO group_owner_tbl values ('operator','operator-group');
+INSERT IGNORE INTO user_tbl (userid, password, domain, flags, fullname, address, addnl_info, unavail_xferto, gatekeeper, password_updated) 
 	VALUES ('messagebox',password('a1b2c3d4'),'ace',0,'Message service feature','','Message service feature - handles offline messaging','', '', NOW());
 
-insert into canned_message_tbl (grp, description, message)
+INSERT IGNORE INTO canned_message_tbl (grp, description, message)
 	values ('operator-group', 'QUIK Computing greetings message',
 		'Welcome to QUIK Computing operator services demo. How may I help you?');
 
-insert into canned_message_tbl (grp, description, message)
+INSERT IGNORE INTO canned_message_tbl (grp, description, message)
 	values ('operator-group', 'QUIK Computing web page',
 		'<a href=http://www.quik-computing.com target=blank_>Click here to visit our web site</href>');
 
-insert into canned_message_tbl (grp, description, message)
+INSERT IGNORE INTO canned_message_tbl (grp, description, message)
 	values ('operator-group', 'QUIK Computing e-mail feedback',
 		'<a href=mailto:info@quik-j.com?subject=Feedback target=_blank>Click here to send us a feedback by email</a>');
 
-insert into feature_tbl values (NULL, 'operator', 'ace', 'com.quikj.application.web.talk.feature.operator.Operator', 1);
-insert into feature_params_tbl values (LAST_INSERT_ID(), 'max-sessions', '1');
-insert into feature_params_tbl values (LAST_INSERT_ID(), 'max-operators', '10');
-insert into feature_params_tbl values (LAST_INSERT_ID(), 'max-queue-size', '20');
-insert into feature_params_tbl values (LAST_INSERT_ID(), 'display-wait-time', 'true');
+INSERT IGNORE INTO feature_tbl values (NULL, 'operator', 'ace', 'com.quikj.application.web.talk.feature.operator.Operator', 1);
+INSERT IGNORE INTO feature_params_tbl values (LAST_INSERT_ID(), 'max-sessions', '1');
+INSERT IGNORE INTO feature_params_tbl values (LAST_INSERT_ID(), 'max-operators', '10');
+INSERT IGNORE INTO feature_params_tbl values (LAST_INSERT_ID(), 'max-queue-size', '20');
+INSERT IGNORE INTO feature_params_tbl values (LAST_INSERT_ID(), 'display-wait-time', 'true');
 
-insert into feature_tbl values (NULL, 'messagebox', 'ace', 'com.quikj.application.web.talk.feature.messagebox.server.MessageBox', 1);
-insert into feature_params_tbl values (LAST_INSERT_ID(), 'from', 'messagebox@mydomain.com');
+INSERT IGNORE INTO feature_tbl values (NULL, 'messagebox', 'ace', 'com.quikj.application.web.talk.feature.messagebox.server.MessageBox', 1);
+INSERT IGNORE INTO feature_params_tbl values (LAST_INSERT_ID(), 'from', 'messagebox@mydomain.com');
