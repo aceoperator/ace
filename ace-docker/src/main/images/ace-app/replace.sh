57f76c99cd2c69b78c14f69a1f2df0fbc22e5df0
Amit Chatterjee <amit.chatterjee@quik-j.com>
2018-11-24 21:34:09 -0500

    Added replace script

---
+++ ace-docker/src/main/images/ace-app/replace.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pattern=\'"$1"\'
+command="env | grep $pattern"
+env_list=`eval $command`
+
+exp_buffer=
+for env in $env_list; do
+    sed_exp=`echo $env | awk -F '=' '{printf "s/\\\\$\\\\$ACE(%s)/%s/g", $1, $2}'`
+    if [ -n "$exp_buffer" ]; then
+        exp_buffer=$exp_buffer";"
+    fi
+    exp_buffer=$exp_buffer$sed_exp    
+done
+
+shift
+eval sed -e \'$exp_buffer\' "$*"
