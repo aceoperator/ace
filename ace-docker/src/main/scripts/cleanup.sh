c12ea536624bae6ac0a454ccc95f511ff7d3dd9b
Amit Chatterjee <amit.chatterjee@quik-j.com>
2018-11-29 09:00:53 -0500

    Reorganized the docker project

--- ace-docker/src/main/scripts/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker stop `docker ps  | awk '{print $1}' | grep --invert-match CONTAINER`
-docker rm `docker ps -a  | awk '{print $1}' | grep --invert-match CONTAINER`
-docker rmi `docker images | grep aceoperator | awk '{print $1":"$2}'`	
-docker images
